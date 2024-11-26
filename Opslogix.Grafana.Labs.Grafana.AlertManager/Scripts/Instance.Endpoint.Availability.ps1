a8be06579616c4acd4a89e3696904f85f92d9689
Jonas Lenntun <jonas@opslogix.com>
2024-11-26 13:25:07 +0700

    Added docs
    And cleaned up script

--- Opslogix.Grafana.Labs.Grafana.AlertManager/Scripts/Instance.Endpoint.Availability.ps1
+++ Opslogix.Grafana.Labs.Grafana.AlertManager/Scripts/Instance.Endpoint.Availability.ps1
@@ -1,8 +1,52 @@
-﻿#Insert parameters if necessary, otherwise remove this
+﻿<#
+.SYNOPSIS
+    This script checks the availability of a specified endpoint by making HTTP GET requests.
+
+.DESCRIPTION
+    The script attempts to connect to a specified URL endpoint multiple times (up to a maximum number of attempts) and logs the results. 
+    It uses the Invoke-RestMethod cmdlet to make HTTP GET requests to the endpoint and handles any connection errors.
+
+.PARAMETER QueryUser
+    The username for authentication (if required).
+
+.PARAMETER QueryPwd
+    The password for authentication (if required).
+
+.PARAMETER URL
+    The base URL of the endpoint to check.
+
+.FUNCTIONS
+    Write-Event
+        Logs an event with a specified EventID, Severity, and Message.
+
+    Write-InfoEvent
+        Logs an informational event with a specified EventID and Message.
+
+    Write-WarningEvent
+        Logs a warning event with a specified EventID and Message.
+
+    Write-ErrorEvent
+        Logs an error event with a specified EventID and Message.
+
+    Exit-Script
+        Logs the script output and exits the script.
+
+.NOTES
+    Version: 1.0
+    Author: [Your Name]
+    This script is part of the Opslogix Grafana Labs Grafana AlertManager integration.
+
+.EXAMPLE
+    .\Instance.Endpoint.Availability.ps1 -URL "http://example.com"
+
+    This command runs the script to check the availability of the endpoint at "http://example.com".
+
+#>
+#Insert parameters if necessary, otherwise remove this
 param(
-$QueryUser,
-$QueryPwd,
-[string]$URL
+    $QueryUser,
+    $QueryPwd,
+    [string]$URL
 )
 
 ### Define Operations Manager objects ###
@@ -16,27 +60,22 @@ $scriptOutput = ""
 #Insert Variables
 
 
-function Write-Event($EventID, $Severity, $Message)
-{
+function Write-Event($EventID, $Severity, $Message) {
     $momScriptAPI.LogScriptEvent($scriptName, $EventID, $Severity, "Version: $version`n$Message")
 }
-function Write-InfoEvent($EventID, $Message)
-{
+function Write-InfoEvent($EventID, $Message) {
     Write-Event -EventID $EventID -Severity 0 -Message $Message
 }
-function Write-WarningEvent($EventID, $Message)
-{
+function Write-WarningEvent($EventID, $Message) {
     Write-Event -EventID $EventID -Severity 2 -Message $Message
 }
-function Write-ErrorEvent($EventID, $Message)
-{
-	Write-Event -EventID $EventID -Severity 1 -Message $Message
+function Write-ErrorEvent($EventID, $Message) {
+    Write-Event -EventID $EventID -Severity 1 -Message $Message
 }
-function Exit-Script()
-{
-	#EventID should be changed
+function Exit-Script() {
+    #EventID should be changed
     Write-InfoEvent -EventID 5380 -Message $scriptOutput
-	exit
+    exit
 }
 
 #EventID should be changed
@@ -73,7 +112,7 @@ $headers = @{
 # create Uri
 $baseurl = $URL
 $resourceurl = "/api/health"
-$uri = $baseurl+ $resourceurl
+$uri = $baseurl + $resourceurl
 
 
 # Get share information but output only name, path and description
@@ -90,42 +129,42 @@ $maxattempts = 3
 
 #Connect to Web API in a while loop
 while ($passed -ne $null -and $attempt -lt $maxattempts) {
-        $attempt += 1
-        $scriptOutput += "Number of Attempts: $attempt`n"
-        sleep 10
+    $attempt += 1
+    $scriptOutput += "Number of Attempts: $attempt`n"
+    sleep 10
 
-        try{
-            ## Try to connect to API
-            $scriptOutput += "Trying to connect to: $uri`n"
-			#Request data from WebAPI
-			$testResult = Invoke-RestMethod -Uri $uri -Method Get
+    try {
+        ## Try to connect to API
+        $scriptOutput += "Trying to connect to: $uri`n"
+        #Request data from WebAPI
+        $testResult = Invoke-RestMethod -Uri $uri -Method Get
 
-            # test response
-            $passed = $testResult
-            $propertyBag.AddValue('Result',"OK")
-			$scriptOutput += "Connection to API a success`n"
+        # test response
+        $passed = $testResult
+        $propertyBag.AddValue('Result', "OK")
+        $scriptOutput += "Connection to API a success`n"
 
-        }#try
+    }#try
 
-        catch{
-            if ($attempt -ge $maxattempts){
-                $propertyBag.AddValue('Result',"CRITICAL")
-				$propertyBag.AddValue('ErrorMessage', "$_")
-				$scriptOutput += "Connection to API $URL FAILED`n"
-				$scriptOutput += "ScriptResult: BAD`n"
-				$scriptOutput += "Error: $_`n"
+    catch {
+        if ($attempt -ge $maxattempts) {
+            $propertyBag.AddValue('Result', "CRITICAL")
+            $propertyBag.AddValue('ErrorMessage', "$_")
+            $scriptOutput += "Connection to API $URL FAILED`n"
+            $scriptOutput += "ScriptResult: BAD`n"
+            $scriptOutput += "Error: $_`n"
 
 
                 
                         
-            }#if
-        }#catch
-    }#while
+        }#if
+    }#catch
+}#while
 
 
 ### Add property bag values ###
-$propertybag.AddValue('Attempt',"$attempt")
-$propertybag.AddValue('URL',"$URL")
+$propertybag.AddValue('Attempt', "$attempt")
+$propertybag.AddValue('URL', "$URL")
 
 ### Return property bag
 $propertyBag
